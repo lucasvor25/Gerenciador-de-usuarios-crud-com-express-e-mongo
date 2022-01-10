2ddae43c3fee674dfd2616cac56598121e128967
Your Name <you@example.com>
2022-01-10 18:38:37 -0300

    alteracoes 3

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 dotenv.config({path: 'config.env'})
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 //log request
 app.use(morgan('tiny'))
