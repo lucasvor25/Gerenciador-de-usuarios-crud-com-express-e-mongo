b399b90d5b9a309ca862c01781767282be1f3fae
Your Name <you@example.com>
2022-01-11 12:26:48 -0300

    tentando

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const api = require('../../api')
 
 
 exports.homeRoutes = (req,res) => {
-api.get('http://localhost:3000/api/users')
+api.get('https://crud-online-nodejs-mongodb.herokuapp.com/api/users')
 .then(function(response) {
     console.log(response.data)
     res.render('index', {users:response.data})
@@ -18,7 +18,7 @@ exports.add_user = (req, res) => {
     res.render('add_user')
 }
 exports.update_user = (req, res) => {
-    api.get('http://localhost:3000/api/users', {params: {id: req.query.id}})
+    api.get('https://crud-online-nodejs-mongodb.herokuapp.com/api/users', {params: {id: req.query.id}})
     .then(function(userdata) {
         res.render('update_user', {user: userdata.data})
     }).catch(err => {
